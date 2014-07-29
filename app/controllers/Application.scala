e03606a3b67c6238b25b251d7c6bae0ebb1dd7e6
zdogma <z.dogma.asaki@gmail.com>
2014-07-30 01:17:01 +0900

    フォームの実装

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,31 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms._
 
 object Application extends Controller {
 
+  case class MyFormData(name:String, mail:String)
+  val form1 = Form(
+    mapping(
+      "name" -> text,
+      "mail" -> text
+    ) (MyFormData.apply) (MyFormData.unapply)
+  )
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val title = "Sample Page"
+    val msg   = "This is a sample page."
+    Ok(views.html.index(title, msg, form1))
+  }
+
+  def sendform = Action { implicit request =>
+    var myForm = form1.bindFromRequest
+    val data: MyFormData = myForm.get
+    val title = "Sample Page"
+    val msg   = "Name: " + data.name + ", Mail: " + data.mail
+    Ok(views.html.index(title, msg, myForm))
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -11,5 +11,6 @@ libraryDependencies ++= Seq(
   anorm,
   cache,
   ws,
-  "mysql" % "mysql-connector-java" % "5.1.24" 
+  "org.slf4j" % "slf4j-nop" % "1.6.4",
+  "mysql" % "mysql-connector-java" % "5.1.24"
 )
